926980def8cbeec0a75dea351e0d1e7d86556cb9
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-26 19:33:11 -0500

    Welcome to Hell!

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,25 @@
 public class DmvSimulator {
     public static void main(String[] args) {
         
+       int number =(int) (Math.random() * 100)+ 1;
+       int trollNumber = number + 1;
+       System.out.println(String.format("Welcome to hell! Your number is %d. Please wait until your number is called.", number));
+       
+       System.out.println(number);
+
+       while (number != trollNumber) {
+            System.out.println(trollNumber);
+
+            trollNumber++;
+
+            if (trollNumber > 100) {
+                trollNumber = 1;
+            }
+       }
+    
+       System.out.println("You don't have the required paperwork. You're supposed to have today's WORDLE, a google-generated password, and a completed 100x100 Sudoku. Get out.");
+
     }
 }
+
+
